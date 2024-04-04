73ccfa3a941d41c491c99a395c36439fe28159f8
Daniele <danieleluongo57@gmail.com>
2024-04-04 22:13:30 +0200

    aggiunto script per popolare db neo4j

---
+++ neo4j/load_books.py
@@ -0,0 +1,21 @@
+# MEGLIO ESEGUIRLO SOLO UNA VOLTA
+
+import os
+from pymongo.mongo_client import MongoClient
+from neo4j import GraphDatabase
+from dotenv import load_dotenv
+load_dotenv()
+
+neo_uri = os.getenv('NEO4J_URI')
+neo_auth = (os.getenv('NEO4J_USERNAME'), os.getenv('NEO4J_PASSWORD'))
+neo_drive = GraphDatabase.driver(neo_uri, auth=neo_auth)
+mongo_client = MongoClient(os.getenv('MONGO_URI'))
+mongo_db = mongo_client.nosqlproject
+mongo_collection = mongo_db.books
+
+mongo_result = mongo_collection.find({}, {'book_title'})
+for result in mongo_result:
+    neo_drive.execute_query(
+    "CREATE (b:BOOK {titolo:$titolo})",
+    titolo = result['book_title']
+    )
